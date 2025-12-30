049151a8240a31a0e36c7abb3ca037eed6fb941e
nadiashahzanani <nadiashahzanani123@gmail.com>
2025-12-30 10:00:57 +0800

    Create member_impulse_factors.py

---
+++ member_impulse_factors.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_excel("responses.xlsx")
+
+st.title("Key Factors Influencing Students’ Impulse Buying on TikTok Shop")
